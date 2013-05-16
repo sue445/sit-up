5336ea49ad9203a9b2810e2320c1237ab49711d4
sue445 <sue445@sue445.net>
2013-05-16 20:16:55 +0900

    impl GenerateHash methods

--- enumerable/lib/generate_hash.rb
+++ enumerable/lib/generate_hash.rb
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 class GenerateHash
   def self.method_foo(source_array)
+    hash = {}
+    source_array.each_slice(2) do |array|
+      hash[array[0]] = array[1]
+    end
+    hash
   end
 
   def self.method_bar(source_array)
+    method_foo(source_array.flatten)
   end
 
   def self.method_baz(keys_array, values_array)
+    hash = {}
+    keys_array.length.times do |i|
+      hash[keys_array[i]] = values_array[i]
+    end
+    hash
   end
 end
